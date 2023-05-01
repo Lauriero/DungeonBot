ea7a487bc4ec81b3a5ad123512e2cd9d57b2c57e
Lauriero <incorobots@gmail.com>
2023-05-01 13:26:22 +0200

    fix: exception handler was not applied to every interaction

--- DungeonDiscordBot/InteractionModules/MusicModule.cs
+++ DungeonDiscordBot/InteractionModules/MusicModule.cs
@@ -42,46 +42,48 @@ public class MusicModule : InteractionModuleBase<SocketInteractionContext>
         MusicProvider? provider = null
     )
     {
-        provider ??= MusicProvider.VK;
-        await DeferAsync();
-        
-        if (Uri.TryCreate(query, UriKind.Absolute, out Uri? link)
-            && (link.Scheme == Uri.UriSchemeHttp || link.Scheme == Uri.UriSchemeHttps)) {
+        await OnExceptionWrapper(async () => {
+            provider ??= MusicProvider.VK;
+            await DeferAsync();
+            
+            if (Uri.TryCreate(query, UriKind.Absolute, out Uri? link)
+                && (link.Scheme == Uri.UriSchemeHttp || link.Scheme == Uri.UriSchemeHttps)) {
 
-            BaseMusicProviderController? controller = link.FindMusicProviderController();
-            if (controller is null) {
-                await RespondAsync("***This link is not supported***");
-                return;
+                BaseMusicProviderController? controller = link.FindMusicProviderController();
+                if (controller is null) {
+                    await RespondAsync("***This link is not supported***");
+                    return;
+                }
+
+                SocketVoiceChannel? targetChannel = GetVoiceChannelWithCurrentUser();
+                if (targetChannel is null) {
+                    await RespondAsync("User is not found in any of the voice channels");
+                    return;
+                }
+                
+                controller.AudiosProcessingStarted += async (audiosCount) => {
+                    await ModifyOriginalResponseAsync(m => m.Content = $"Processing 0/{audiosCount}");
+                };
+
+                controller.AudiosProcessingProgressed += async (audiosProcessed, audiosCount) => {
+                    await ModifyOriginalResponseAsync(m => m.Content = $"Processing {audiosProcessed}/{audiosCount}");
+                };
+
+                controller.AudiosProcessed += async (audiosProcessed) => {
+                    await ModifyOriginalResponseAsync(m => m.Content = $"{audiosProcessed} audios were processed");
+                };
+                
+                IEnumerable<AudioQueueRecord> records = await controller.GetAudiosFromLink(link);
+
+                _aggregator.DiscordAudio.RegisterChannel(Context.Guild, targetChannel.Id);
+                _aggregator.DiscordAudio.AddAudios(Context.Guild.Id, records);
+                
+                await ModifyOriginalResponseAsync((m) => m.Content = $"Queue started");
+                await _aggregator.DiscordAudio.PlayQueueAsync(Context.Guild.Id);
+            } else {
+                await RespondAsync("Searching...");
             }
-
-            SocketVoiceChannel? targetChannel = GetVoiceChannelWithCurrentUser();
-            if (targetChannel is null) {
-                await RespondAsync("User is not found in any of the voice channels");
-                return;
-            }
-            
-            controller.AudiosProcessingStarted += async (audiosCount) => {
-                await ModifyOriginalResponseAsync(m => m.Content = $"Processing 0/{audiosCount}");
-            };
-
-            controller.AudiosProcessingProgressed += async (audiosProcessed, audiosCount) => {
-                await ModifyOriginalResponseAsync(m => m.Content = $"Processing {audiosProcessed}/{audiosCount}");
-            };
-
-            controller.AudiosProcessed += async (audiosProcessed) => {
-                await ModifyOriginalResponseAsync(m => m.Content = $"{audiosProcessed} audios were processed");
-            };
-            
-            IEnumerable<AudioQueueRecord> records = await controller.GetAudiosFromLink(link);
-
-            _aggregator.DiscordAudio.RegisterChannel(Context.Guild, targetChannel.Id);
-            _aggregator.DiscordAudio.AddAudios(Context.Guild.Id, records);
-            
-            await ModifyOriginalResponseAsync((m) => m.Content = $"Queue started");
-            await _aggregator.DiscordAudio.PlayQueueAsync(Context.Guild.Id);
-        } else {
-            await RespondAsync("Searching...");
-        }
+        });
     }
 
     [SlashCommand("stop", "Stops playing the songs queue", runMode: RunMode.Async)]
@@ -98,18 +100,22 @@ public class MusicModule : InteractionModuleBase<SocketInteractionContext>
     [SlashCommand("queue", "Shows the list of songs that are currently playing", runMode: RunMode.Async)]
     public async Task ShowQueueAsync()
     {
-        await DeferAsync();
-        
-        ConcurrentQueue<AudioQueueRecord> queue = _aggregator.DiscordAudio.GetQueue(Context.Guild.Id);
-        await ModifyOriginalResponseAsync(m => m.GenerateQueueMessage(queue));
+        await OnExceptionWrapper(async () => {
+            await DeferAsync();
+            
+            ConcurrentQueue<AudioQueueRecord> queue = _aggregator.DiscordAudio.GetQueue(Context.Guild.Id);
+            await ModifyOriginalResponseAsync(m => m.GenerateQueueMessage(queue));
+        });
     }
 
     [SlashCommand("shuffle", "Shuffles the list of songs", runMode: RunMode.Async)]
     public async Task ShuffleQueueAsync()
     {
-        await DeferAsync();
-        _aggregator.DiscordAudio.ShuffleQueue(Context.Guild.Id);
-        await ModifyOriginalResponseAsync(m => m.Content = "Queue is shuffled");
+        await OnExceptionWrapper(async () => {
+            await DeferAsync();
+            _aggregator.DiscordAudio.ShuffleQueue(Context.Guild.Id);
+            await ModifyOriginalResponseAsync(m => m.Content = "Queue is shuffled");
+        });
     }
 
     private async Task OnExceptionWrapper(Func<Task> inner)
