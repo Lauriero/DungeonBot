722362060b0946e467837f25e79a0f8bdc6fd93f
Lauriero <incorobots@gmail.com>
2023-12-14 20:57:52 +0100

    feat: add autocomplete handler for music queries

---
+++ DungeonDiscordBot/AutocompleteHandlers/QueryAutocompleteHandler.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.Interactions;
+
+using DungeonDiscordBot.Controllers.Abstraction;
+using DungeonDiscordBot.Model.Database;
+
+namespace DungeonDiscordBot.AutocompleteHandlers;
+
+public class QueryAutocompleteHandler : AutocompleteHandler
+{
+    private readonly IDataStorageService _dataStorage;
+    
+    public QueryAutocompleteHandler(IDataStorageService dataStorage)
+    {
+        _dataStorage = dataStorage;
+    }
+    
+    public override async Task<AutocompletionResult> GenerateSuggestionsAsync(IInteractionContext context, IAutocompleteInteraction autocompleteInteraction,
+        IParameterInfo parameter, IServiceProvider services)
+    {
+        IEnumerable<MusicQueryHistoryEntity> results = await _dataStorage.GetLastMusicQueries(context.Guild.Id);
+        return AutocompletionResult.FromSuccess(results
+            .Select(q => new AutocompleteResult(q.QueryName, q.QueryValue))
+            .Take(25));
+    }
+}
